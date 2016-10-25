e7e5cfaa9781f627f55c9e4281ab6fbedeb1d433
Sophie Gill <sophie-gill@hotmail.com>
2016-10-25 17:41:01 +0100

    refactors app.rb to put logic into controller

--- app/app.rb
+++ app/app.rb
@@ -3,26 +3,16 @@ ENV["RACK_ENV"] ||= "development"
 require 'sinatra/base'
 require 'sinatra/flash'
 
-require_relative 'datamapper_setup'
 
 class Peachio < Sinatra::Base
 
   register Sinatra::Flash
 
-  get '/' do
-    erb :'leads/new'
-  end
-
-  post '/' do
-    lead = Lead.new(email: params[:email])
-    if lead.save
-      erb :'leads/index'
-    else
-      flash.now[:errors] = lead.errors.full_messages
-      erb :'leads/new'
-    end
-  end
+  require_relative 'datamapper_setup'
+  require_relative 'server'
+  require_relative 'controllers/leads'
 
   # start the server if ruby file executed directly
   run! if app_file == $0
+
 end
